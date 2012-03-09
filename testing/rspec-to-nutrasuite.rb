349e961c1951adff6bea6028e333f389bf9bf778
Graham Ashton <graham@effectif.com>
2012-03-09 16:40:41 +0000

    Started an RSpec to nutrasuite converter.

---
+++ testing/rspec-to-nutrasuite.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+#
+# Does most of the work required to convert RSpec tests to use MiniTest
+# and Nutrasuite.
+#
+# Usage: rspec-to-nutrasuite.rb < spec/thing_spec.rb > test/thing_test.rb
+
+def convert_require_statements(line)
+  line.sub /^(require.*)(spec)/, '\1test'
+end
+
+def convert_top_level_describe_to_class(line)
+  if line =~ /^describe (["'](.+)["']|(\w+)) do/
+    testable = ($2.to_s + $3.to_s).split.map { |word| word.capitalize }.join
+    "class #{testable}Test < MiniTest::Unit::TestCase"
+  else
+    line
+  end
+end
+
+def convert_nested_describe(line)
+  line.sub /^(\s+)describe/, '\1the'
+end
+
+def convert_before_and_after(line)
+  line.sub /(before|after)[(\s]+:each\)?/, '\1'
+end
+
+ARGF.each_line do |line|
+  puts [
+    :convert_require_statements,
+    :convert_top_level_describe_to_class,
+    :convert_nested_describe,
+    :convert_before_and_after
+  ].inject(line) { |line, method| send(method, line) }
+end
+
