ebadea0895dc7b5c1886e68430c378fedb63ddc7
Graham Ashton <graham@effectif.com>
2012-12-17 16:26:26 +0000

    Check sites 5 times before throwing an error.

--- bin/ping-site.sh
+++ bin/ping-site.sh
@@ -6,14 +6,18 @@ SITE="${1:-http://effectif.com}"
 
 site_is_up()
 {
-    curl -I -f $SITE 2>/dev/null | head -n 1 | grep -qs 200
+    curl -k -I -f $SITE 2>/dev/null | head -n 1 | grep -qs 200
 }
 
 ## Main program
 
 [ -n "$DEBUG" ] && set -x
 
-site_is_up
+for i in $(seq 5); do
+    if site_is_up; then
+        exit 0
+    fi
+done
 
 if [ $? -ne 0 ]; then
     curl -I $SITE
